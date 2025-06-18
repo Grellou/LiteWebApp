47830aada513addcbdc45928720d5b56c0baf84f
Grellou <dolbylarrens@gmail.com>
2025-06-18 19:41:20 +0300

    Store management. Search and sorting.

--- app/models.py
+++ app/models.py
@@ -1,14 +1,6 @@
 from app import db, bcrypt
 from flask_login import UserMixin
 
-class ItemModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), nullable=False)
-    type = db.Column(db.String(20), nullable=False)
-    language = db.Column(db.String(20), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-
 class UserModel(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
@@ -25,3 +17,11 @@ class UserModel(db.Model, UserMixin):
     # Check hashed password
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password_hash, password)
+
+class ItemModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    type = db.Column(db.String(20), nullable=False)
+    language = db.Column(db.String(20), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
--- app/routes/nagivation_route.py
+++ app/routes/nagivation_route.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from app.models import ItemModel
+from sqlalchemy import or_
 
 bp = Blueprint("navigation", __name__)
 
@@ -12,6 +13,23 @@ def home_page():
 # Store page route
 @bp.route("/store")
 def store_page():
-    items = ItemModel.query.all()
-    return render_template("store.html", items=items)
+    search_query = request.args.get("query", "") # search query
+    sort = request.args.get("sort", "name")
+    direction = request.args.get("direction", "asc")
+
+    # Search
+    if search_query:
+        items = ItemModel.query.filter(or_(ItemModel.name.ilike(f"%{search_query}%"))).all()
+    else:
+        items = ItemModel.query.all()
+
+    # Sorting   
+    sort_column = getattr(ItemModel, sort, ItemModel.name)
+    if direction == "desc":
+        sort_column = sort_column.desc()
+    else:
+        sort_column = sort_column.asc()
+    items = ItemModel.query.order_by(sort_column).all()
+
+    return render_template("store.html", items=items, sort=sort, direction=direction, search_query=search_query)
 
